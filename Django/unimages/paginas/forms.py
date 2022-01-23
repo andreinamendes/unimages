a9741c8193ed79484409e8fd7e09abd7cfeb7564
Elano Nunes <53663166+elanonc@users.noreply.github.com>
2022-01-23 00:09:54 -0300

    Assinante e Favoritar Imagem

--- Django/unimages/paginas/forms.py
+++ Django/unimages/paginas/forms.py
@@ -23,6 +23,14 @@ class PlanoForm(forms.ModelForm):
         duracao = forms.CharField()
         pix = forms.CharField()
 
+class AssinanteForm(forms.ModelForm):
+    class Meta:
+        model = Assinante
+        fields = ['data_de_inicio', 'data_final', 'plano']
+        data_de_inicio = forms.DateField()
+        data_final = forms.DateField()
+        plano = forms.CharField()
+
 
 class ImagemForm(forms.ModelForm):
     class Meta:
