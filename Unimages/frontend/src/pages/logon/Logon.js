edccf94ed97ff1c33282153341ac8d40a8c96548
Elano Nunes <53663166+elanonc@users.noreply.github.com>
2022-01-26 18:53:17 -0300

    Goodbye

--- Unimages/frontend/src/pages/logon/Logon.js
+++
@@ -1,7 +0,0 @@
-import { Link } from 'react-router-dom'
-
-export function Logon(props) {
-    return (
-        <div>teste</div>
-    );
-}
--- Unimages/frontend/src/pages/profile/Profile.js
+++
@@ -1,7 +0,0 @@
-export function Profile(props) {
-    return (
-        <section>
-            <h1>Perfil</h1>
-        </section>
-    );
-}
