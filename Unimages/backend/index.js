ccaad7f8fa3d05e86d255ae5fe9ec5bcbab74dd2
João Victor <48702646+jvac99@users.noreply.github.com>
2022-01-27 09:33:19 -0300

    at

--- Unimages/backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = require('./src/routes');
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-app.listen(3333, () => console.log('Server listening on port 3333'));
