1c19da96163f713097e9e0c3e0629a8ecaada872
João Victor <48702646+jvac99@users.noreply.github.com>
2022-01-28 03:09:51 -0300

    planos at

--- Django/unimages/paginas/views.py
+++ Django/unimages/paginas/views.py
@@ -16,24 +16,36 @@ from random import shuffle
 
 @login_required
 def index(request):
-    imagens = listar_imagens(request)
-    categorias = listar_categoria_imagens(request)
-    context = {'imagens': imagens, 'categorias': categorias}
-    return render(request, 'paginas/index.html', context)
+    try:
+        imagens = listar_imagens(request)
+        categorias = listar_categoria_imagens(request)
+        context = {'imagens': imagens, 'categorias': categorias}
+        return render(request, 'paginas/index.html', context)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Nenhuma imagem encontrada.')
 
 
 @login_required
 def listar_imagens(request):
-    imagens = list(Imagem.objects.all())
-    shuffle(imagens)
-    return imagens
+    try:
+        imagens = list(Imagem.objects.all())
+        shuffle(imagens)
+        return imagens
+    except:
+        redirect('/home')
+        messages.warning(request, 'Nenhuma imagem encontrada.')
 
 
 @login_required
 def listar_imagens_favoritas(request):
-    imagens = list(Imagem_favorita.objects.all())
-    shuffle(imagens)
-    return imagens
+    try:
+        imagens = list(Imagem_favorita.objects.all())
+        shuffle(imagens)
+        return imagens
+    except:
+        redirect('/home')
+        messages.warning(request, 'Nenhuma imagem encontrada.')
 
 
 @login_required
@@ -72,7 +84,6 @@ def contatos(request):
     context = {'imagens': imagens, 'categorias': categorias, 'form': form}
     return render(request, 'paginas/contatos.html', context)
 
-
 @login_required
 def planos(request):
     planos = listar_planos(request)
@@ -84,55 +95,80 @@ def planos(request):
 
 @login_required
 def listar_planos(request):
-    return Plano.objects.all().order_by(
-        '-created_at')
+    try:
+        return Plano.objects.all().order_by(
+            '-created_at')
+    except:
+        redirect('/home')
+        messages.warning(request, 'Nenhum plano encontrado.')
 
 
 def plano(request, id):
-    plano = get_object_or_404(Plano, pk=id)
-    return render(request, 'paginas/plano/plano.html', {'plano': plano})
+    try:
+        plano = get_object_or_404(Plano, pk=id)
+        return render(request, 'paginas/plano/plano.html', {'plano': plano})
+    except:
+        redirect('/home')
+        messages.warning(request, 'Plano não encontado.')
 
 
 @login_required
 def cadastrar_plano(request):
-    if request.method == 'POST':
-        form = PlanoForm(request.POST)
-        if form.is_valid():
-            plano = form.save(commit=False)
-            plano.usuario = request.user
-            plano.save()
-            messages.info(request, 'Plano salvo com sucesso.')
-            return redirect('/planos')
+    try:
+        if request.method == 'POST':
+            form = PlanoForm(request.POST)
+            if form.is_valid():
+                plano = form.save(commit=False)
+                plano.usuario = request.user
+                plano.save()
+                messages.info(request, 'Plano salvo com sucesso.')
+                return redirect('/planos')
+            else:
+                # ser_autor
+                return render(request, 'paginas/plano/cad_plano.html', {'form': form})
         else:
+            form = PlanoForm()
             # ser_autor
             return render(request, 'paginas/plano/cad_plano.html', {'form': form})
-    else:
-        form = PlanoForm()
-        # ser_autor
-        return render(request, 'paginas/plano/cad_plano.html', {'form': form})
+    except:
+        redirect('/home')
+        messages.warning(request, 'Erro ao cadastrar plano.')
 
 
 @login_required
 def editar_plano(request, id):
-    plano = get_object_or_404(Plano, id=id)
-    if request.method == 'POST':
-        form = PlanoForm(request.POST, instance=plano)
-        if form.is_valid():
-            plano = form.save(commit=False)
-            plano.usuario = request.user
-            plano.save()
-            messages.info(request, 'Plano salvo com sucesso.')
-            return redirect('/planos')
+    try:
+        plano = get_object_or_404(Plano, id=id)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Plano não encontado.')
+        
+    try:
+        if request.method == 'POST':
+            form = PlanoForm(request.POST, instance=plano)
+            if form.is_valid():
+                plano = form.save(commit=False)
+                plano.usuario = request.user
+                plano.save()
+                messages.info(request, 'Plano salvo com sucesso.')
+                return redirect('/planos')
+            else:
+                return render(request, 'paginas/plano/editar_plano.html', {'form': form})
         else:
+            form = PlanoForm(instance=plano)
             return render(request, 'paginas/plano/editar_plano.html', {'form': form})
-    else:
-        form = PlanoForm(instance=plano)
-        return render(request, 'paginas/plano/editar_plano.html', {'form': form})
-
+    except:
+        redirect('/home')
+        messages.warning(request, 'Plano não editado.')
+    
 
 @login_required
 def deletar_plano(request, id):
-    plano = get_object_or_404(Plano, id=id)
+    try:
+        plano = get_object_or_404(Plano, id=id)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Plano não encontado.')
     plano.delete()  # Deletando o plano
     messages.info(request, 'Plano deletado com sucesso.')
     return redirect('/planos')
@@ -140,13 +176,12 @@ def deletar_plano(request, id):
 
 @login_required
 def autores(request):
-    return render(request, 'paginas/autores.html')
+    pass
 
 
 @login_required
 def autor(request):
-
-    return render(request, 'paginas/autor/autor.html')
+    pass
 
 
 @login_required
@@ -155,47 +190,63 @@ def listar_autores(request):
 
 
 def listar_autor(request, id):
-    return get_object_or_404(Autor, pk=id)  # Buscando o quadro
+    pass  # Buscando o quadro
 
 
 @login_required
 def cadastrar_autor(request):
-    if request.method == 'POST':
-        form = AutorForm(request.POST)
-        if form.is_valid():
-            autor = form.save(commit=False)
-            autor.usuario = request.user
-            autor.save()
-            messages.info(request, 'Autor salvo com sucesso.')
-            return redirect('/home')
+    try:
+        if request.method == 'POST':
+            form = AutorForm(request.POST)
+            if form.is_valid():
+                autor = form.save(commit=False)
+                autor.usuario = request.user
+                autor.save()
+                messages.info(request, 'Autor salvo com sucesso.')
+                return redirect('/home')
+            else:
+                return render(request, 'paginas/autor/ser_autor.html', {'form': form})
         else:
+            form = AutorForm()
             return render(request, 'paginas/autor/ser_autor.html', {'form': form})
-    else:
-        form = AutorForm()
-        return render(request, 'paginas/autor/ser_autor.html', {'form': form})
+    except:
+        redirect('/home')
+        messages.warning(request, 'Ouve algum erro no cadastro.')
 
 
 @login_required
 def editar_autor(request):
-    autor = get_object_or_404(Autor, usuario=request.user)
-    if request.method == 'POST':
-        form = AutorForm(request.POST, instance=autor)
-        if form.is_valid():
-            autor = form.save(commit=False)
-            autor.usuario = request.user
-            autor.save()
-            messages.info(request, 'Autor salvo com sucesso.')
-            return redirect('/home')
+    try:
+        autor = get_object_or_404(Autor, usuario=request.user)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Autor não encontado.')
+    try:
+        if request.method == 'POST':
+            form = AutorForm(request.POST, instance=autor)
+            if form.is_valid():
+                autor = form.save(commit=False)
+                autor.usuario = request.user
+                autor.save()
+                messages.info(request, 'Autor salvo com sucesso.')
+                return redirect('/home')
+            else:
+                return render(request, 'paginas/plano/planos.html', {'form': form})
         else:
+            form = AutorForm(instance=autor)
             return render(request, 'paginas/plano/planos.html', {'form': form})
-    else:
-        form = AutorForm(instance=autor)
-        return render(request, 'paginas/plano/planos.html', {'form': form})
+    except:
+        redirect('/home')
+        messages.warning(request, 'Houve um erro ao editar o autor.')
 
 
 @login_required
 def deletar_autor(request):
-    autor = get_object_or_404(Autor, usuario=request.user)
+    try:
+        autor = get_object_or_404(Autor, usuario=request.user)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Autor não encontado.')
     autor.delete()  # Deletando o quadro
     messages.info(request, 'Autor deletado com sucesso.')
     return redirect('/home')
@@ -203,12 +254,20 @@ def deletar_autor(request):
 
 @login_required
 def imagem(request, id):
-    imagem = get_object_or_404(Imagem, id=id)
+    try:
+        imagem = get_object_or_404(Imagem, id=id)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Imagem não encontada.')
     return render(request, 'paginas/imagem/imagem.html', {'imagem': imagem})
 
 
 def listar_imagem(request, id):
-    return get_object_or_404(Imagem, pk=id)  # Buscando o quadro
+    try:
+        return get_object_or_404(Imagem, pk=id)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Imagem não encontada.')
 
 
 @login_required
@@ -248,55 +307,71 @@ def deletar_imagem(request, id):
 
 @login_required
 def cadastrar_categoria_imagem(request):
-    if request.method == 'POST':
-        form = CategoriaImagemForm(request.POST)
-        if form.is_valid():
-            categoria = form.save(commit=False)
-            categoria.save()
-            messages.info(request, 'Categoria salva com sucesso.')
-            return redirect('/home')
+    try:
+        if request.method == 'POST':
+            form = CategoriaImagemForm(request.POST)
+            if form.is_valid():
+                categoria = form.save(commit=False)
+                categoria.save()
+                messages.info(request, 'Categoria salva com sucesso.')
+                return redirect('/home')
+            else:
+                # ser_autor
+                return render(request, 'paginas/categoria/cad_categoria.html', {'form': form})
         else:
+            form = CategoriaImagemForm()
             # ser_autor
             return render(request, 'paginas/categoria/cad_categoria.html', {'form': form})
-    else:
-        form = CategoriaImagemForm()
-        # ser_autor
-        return render(request, 'paginas/categoria/cad_categoria.html', {'form': form})
+    except:
+        redirect('/home')
+        messages.warning(request, 'Houve um erro ao cadastrar a categoria.')
 
 
 @login_required
 def cadastrar_formato_imagem(request):
-    if request.method == 'POST':
-        form = FormatoImagemForm(request.POST)
-        if form.is_valid():
-            formato = form.save(commit=False)
-            formato.save()
-            messages.info(request, 'Formato salvo com sucesso.')
-            return redirect('/home')
+    try:
+        if request.method == 'POST':
+            form = FormatoImagemForm(request.POST)
+            if form.is_valid():
+                formato = form.save(commit=False)
+                formato.save()
+                messages.info(request, 'Formato salvo com sucesso.')
+                return redirect('/home')
+            else:
+                # ser_autor
+                return render(request, 'paginas/formato/cad_formato.html', {'form': form})
         else:
+            form = FormatoImagemForm()
             # ser_autor
             return render(request, 'paginas/formato/cad_formato.html', {'form': form})
-    else:
-        form = FormatoImagemForm()
-        # ser_autor
-        return render(request, 'paginas/formato/cad_formato.html', {'form': form})
+    except:
+        redirect('/home')
+        messages.warning(request, 'Formato não cadastrado.')
 
 
 @login_required
 def favoritar_imagem(request, id):
-    imagem = get_object_or_404(Imagem, id=id)
-    if request.method == 'POST':
-        form = ImagemFavoritaForm(request.POST)
-        if form.is_valid():
-            imagem_favorita = form.save(commit=False)
-            imagem_favorita.imagem = imagem
-            imagem_favorita.usuario = request.user
-            imagem_favorita.save()
-            messages.info(request, 'salva com sucesso.')
-            return redirect('/home')
-        else:
-            messages.info(request, 'Já foi salva.')
-            return redirect('/home')
+    try:
+        imagem = get_object_or_404(Imagem, id=id)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Imagem não encontada.')
+    try:
+        if request.method == 'POST':
+            form = ImagemFavoritaForm(request.POST)
+            if form.is_valid():
+                imagem_favorita = form.save(commit=False)
+                imagem_favorita.imagem = imagem
+                imagem_favorita.usuario = request.user
+                imagem_favorita.save()
+                messages.info(request, 'salva com sucesso.')
+                return redirect('/home')
+            else:
+                messages.info(request, 'Já foi salva.')
+                return redirect('/home')
+    except:
+        redirect('/home')
+        messages.warning(request, 'Imagem não favoritada.')
 
 
 @login_required
@@ -306,46 +381,55 @@ def imagens_favoritas(request):
 
 @login_required
 def cadastrar_assinante(request):
-    if request.method == 'POST':
-        form = AssinanteForm(request.POST)
-        if form.is_valid():
-            assinante = form.save(commit=False)
-            assinante.usuario = request.user
-            assinante.data_de_inicio = date.today()
-            assinante.data_final = date.today() + timedelta(days=364)
-            assinante.save()
-            messages.info(request, 'Parabens! Assinatura feita com sucesso!')
-            return redirect('/home')
+    try:
+        if request.method == 'POST':
+            form = AssinanteForm(request.POST)
+            if form.is_valid():
+                assinante = form.save(commit=False)
+                assinante.usuario = request.user
+                assinante.data_de_inicio = date.today()
+                assinante.data_final = date.today() + timedelta(days=364)
+                assinante.save()
+                messages.info(request, 'Parabens! Assinatura feita com sucesso!')
+                return redirect('/home')
+            else:
+                return render(request, 'paginas/assinante/ser_assinante.html', {'form': form})
         else:
+            form = AssinanteForm()
             return render(request, 'paginas/assinante/ser_assinante.html', {'form': form})
-    else:
-        form = AssinanteForm()
-        return render(request, 'paginas/assinante/ser_assinante.html', {'form': form})
-    # pass
+    except:
+        redirect('/home')
+        messages.warning(request, 'Houve um erro ao cadastrar o assinante.')
 
 
 @login_required
 def estudante(request):
-    #plano = get_object_or_404(Plano, nome="Estudante")
-    if request.method == 'POST':
-        form = EstudanteForm(request.POST, request.FILES)
-        if form.is_valid():
-            estudante = form.save(commit=False)
-            estudante.usuario = request.user
-            #estudante = estudante.plano = plano
-            estudante = estudante.save()
-            messages.info(request, 'Estudante salvo com sucesso.')
-            return redirect('/home')
+    try:
+        if request.method == 'POST':
+            form = EstudanteForm(request.POST, request.FILES)
+            if form.is_valid():
+                estudante = form.save(commit=False)
+                estudante.usuario = request.user
+                estudante = estudante.save()
+                messages.info(request, 'Estudante salvo com sucesso.')
+                return redirect('/home')
+            else:
+                return render(request, 'paginas/estudante/sou_estudante.html', {'form': form})
         else:
+            form = EstudanteForm()
             return render(request, 'paginas/estudante/sou_estudante.html', {'form': form})
-    else:
-        form = EstudanteForm()
-        return render(request, 'paginas/estudante/sou_estudante.html', {'form': form})
+    except:
+        redirect('/home')
+        messages.warning(request, 'O plano de estudante não pôde ser contratado.')
 
 
 @login_required
 def imagens_categorias(request, id):
-    categoria = get_object_or_404(Categoria_imagem, pk=id)
+    try:
+        categoria = get_object_or_404(Categoria_imagem, pk=id)
+    except:
+        redirect('/home')
+        messages.warning(request, 'Categoria não encontada.')
     imagens = list(Imagem.objects.all().filter(categoria=categoria))
     shuffle(imagens)
     categorias = listar_categoria_imagens(request)
